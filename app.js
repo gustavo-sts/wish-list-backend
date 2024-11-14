887f0a9d960e57096cfcf2005192e6fe01ef436c
gustavo <gustavo38000@gmail.com>
2024-11-14 20:22:29 -0300

    Segunda mudança

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const conn = require("./connection/conn");
 const wishesRouter = require("./source/routes/wishes");
 const cors = require("cors");
 
-conn(); // A conexão com o banco de dados
+conn();
 
 app.use(cors());
-app.use(express.json()); // Adicionando o middleware para garantir que o corpo da requisição seja interpretado como JSON
+app.use(express.json());
 
 app.use("/", wishesRouter);
 
